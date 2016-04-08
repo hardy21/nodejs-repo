4f2e5c385b402e5e100924934aacfc92427b34ea
Carla Hardy <hardy21@llnl.gov>
2016-04-08 22:49:34 +0000

    First commit

--- 1-hello.js
+++ 1-hello.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 1337;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(process.env.PORT, process.env.IP, () => {
+  console.log(`Server running!`);
+});
